abd2f285bee120cbe9e03c7a68531ec61e93cafe
AndyCMM <953087547@qq.com>
2019-03-03 12:00:11 +0800

    测试通过的原始代码

--- just_try/core_python/programmer/lession10/3.py
+++ just_try/core_python/programmer/lession10/3.py
@@ -1,22 +1,33 @@
-# coding=utf-8
-def solution(l):
-    l = [1, 3, 2, 1]
+def solution(A):
+    # write your code in Python 3.6
+    l = A
+
     n = len(l)
     k = []
-    for i in range(1, n-1):
-        if l[i-1] < l[i] and l[i] > l[i+1]:
+    for i in range(1, n - 1):
+        if l[i - 1] < l[i] and l[i] > l[i + 1]:
             k.append(i)
-    for i in range(len(k), 1, -1):
-        m = len(l)//i
+    len_k = len(k)
+    for i in Solution(n):
+        if i > len_k:
+            continue
+        m = n / i
         b = 1
         for j in range(len(k)):
-            if k[j] <= b*m - 1:
+            if (b - 1) * m <= k[j] <= b * m - 1:
                 b += 1
-            if b == i and k[-1] > (b - 1) * m - 1:
+            if b == i + 1:
                 return i
-    if len(k) > 0:
-        return 1
+
     return 0
 
 
-print(solution([]))
+def Solution(N):
+    # write your code in Python 3.6
+    a = N
+    l = []
+    while a >= 1:
+        if N % a == 0:
+            l.append(a)
+        a -= 1
+    return l
