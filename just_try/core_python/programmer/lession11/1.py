7aba3fef10973d21e3c794c724b77a6ccf8c3e76
AndyCMM <953087547@qq.com>
2019-03-03 20:06:32 +0800

    3.txt文件修改

--- just_try/core_python/programmer/lession11/1.py
+++ just_try/core_python/programmer/lession11/1.py
@@ -1,1 +1,27 @@
 # coding=utf-8
+
+N = 26
+P = [1,4,16]
+Q = [26,10,20]
+l = [1] * (N + 1)
+for i in range(2, N + 1):
+    if l[i] > 3:
+        continue
+    temp = i + i
+    while temp <= N:
+
+        l[temp] += l[i]
+        temp += i
+a = 0
+print(l)
+for i in range(N + 1):
+    if l[i] == 2 or l[i] == 3:
+        a += 1
+    l[i] = a
+
+for i in range(3):
+    print(l[Q[i]] - l[P[i]-1])
+#print(l)
+#print([i for i in range(N + 1) if l[i] == 2 or l[i] == 3])
+
+
