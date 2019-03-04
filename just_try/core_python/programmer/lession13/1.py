3bdcb9afb1d7c32357a97df6b96c9982c1473d77
AndyCMM <953087547@qq.com>
2019-03-05 00:09:04 +0800

    3.txt文件修改

--- just_try/core_python/programmer/lession13/1.py
+++ just_try/core_python/programmer/lession13/1.py
@@ -1,15 +1,36 @@
 # coding=utf-8
 A = [0] * 11
-A[3] = A[4] = A[6] = 1
+A[2] = A[3] = A[5] = 1
+A = [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]
 L = len(A) + 1
 n = len(A)
-F = [0, 1]
-i = 2
+d = []
+D = 0
+for i in range(len(A)):
+    if A[i] == 1:
+        d.append(i + 1)
+d.append(L)
+F = [0] * (L +1)
+a = 0
+b = 1
 while True:
-    F.append(F[i - 1] + F[i - 2])
-    i += 1
-    if F[-1] >= L:
+    a , b = b, a + b
+    if a > L:
         break
+    F[a] = 1
+for i in range(len(d)):
+    pass
+print(d)
+d.reverse()
+i = 0
+res = 12
+while True:
+    break
+
+
+
+
+print(A)
 print(F)
 
 
